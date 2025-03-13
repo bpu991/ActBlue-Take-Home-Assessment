e922e629fc2eb8345ad059394e96857feeee9a16
Benjamin Pu <bpu991@gmail.com>
2025-03-13 19:38:39 -0400

    Styled and populated the EventInfo component (#4)

--- src/components/EventInformation.js
+++ src/components/EventInformation.js
@@ -1,28 +1,66 @@
 import { Box, Typography } from "@mui/material";
-
-const EventInformation = ({artistName, date, location}) => {
+import CalendarMonthIcon from "@mui/icons-material/CalendarMonth";
+import LocationOnIcon from "@mui/icons-material/LocationOn";
+import { theme } from "..";
+const EventInformation = ({ artistName, date, location }) => {
     
-    return (
-        <Box 
-            height="125px" 
-            bgcolor="papayawhip" 
-            display="flex" 
-            flexDirection="column"
-            justifyContent="center"
-            paddingLeft={4}
-            marginBottom={4}
-        >
-            <Typography variant="h1">
-                {artistName}
-            </Typography>
-            <Typography variant="subtitle1">
-                {date}
-            </Typography>
-            <Typography variant="subtitle1">
-                {location}
-            </Typography>
-        </Box>
-    )
+  const formatDate = (timestamp) => {
+    return new Intl.DateTimeFormat("en-US", {
+      weekday: "long",
+      month: "long",
+      day: "numeric",
+    }).format(new Date(timestamp));
+  };
+
+  const formattedDate = formatDate(date);
+
+  return (
+    <Box
+      display="flex"
+      flexDirection="column"
+      justifyContent="center"
+      marginBottom={4}
+      gap={2}
+      sx={{
+        [theme.breakpoints.down("md")]: {
+          paddingLeft: 4,
+        },
+      }}
+    >
+      <Typography variant="h1">{artistName}</Typography>
+      <Box
+        display="flex"
+        alignItems="center"
+        sx={{
+          wordWrap: "break-word",
+          overflowWrap: "break-word",
+          whiteSpace: "normal",
+          [theme.breakpoints.down("md")]: {
+            alignItems: "flex-start",
+          },
+        }}
+      >
+        <CalendarMonthIcon />
+        <Typography variant="subtitle1">{formattedDate}</Typography>
+      </Box>
+
+      <Box
+        display="flex"
+        alignItems="center"
+        sx={{
+          wordWrap: "break-word",
+          overflowWrap: "break-word",
+          whiteSpace: "normal",
+          [theme.breakpoints.down("md")]: {
+            alignItems: "flex-start",
+          },
+        }}
+      >
+        <LocationOnIcon />
+        <Typography variant="subtitle1">{location}</Typography>
+      </Box>
+    </Box>
+  );
 };
 
 export default EventInformation;
